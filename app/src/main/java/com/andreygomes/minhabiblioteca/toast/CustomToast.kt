ea74926349bcb086c49ad1e64858cf670774859b
Andrey Gomes <andrey.gomes@gmail.com>
2018-10-16 23:03:45 -0300

    Toasts com cores diferentes.

--- app/src/main/java/com/andreygomes/minhabiblioteca/toast/CustomToast.kt
+++ app/src/main/java/com/andreygomes/minhabiblioteca/toast/CustomToast.kt
@@ -9,12 +9,29 @@ import com.andreygomes.minhabiblioteca.R
 
 class CustomToast(val context: Context, val inflater: LayoutInflater) {
 
-    fun showToast(text: String) {
+    companion object {
+        val WARNING = 1
+        val SUCCESS = 2
+        val ERROR = 3
+        val INFO = 4
+    }
+
+    fun showToast(text: String, type: Int) {
         val customToastroot = inflater.inflate(R.layout.custom_toast, null)
         val customtoast = Toast(context)
 
         val textView = customToastroot.findViewById<TextView>(R.id.textView1)
         textView.text = text
+
+        when (type) {
+            WARNING -> customToastroot.setBackgroundResource(R.drawable.mycustom_toast_border_warning)
+            SUCCESS -> customToastroot.setBackgroundResource(R.drawable.mycustom_toast_border_success)
+            ERROR -> customToastroot.setBackgroundResource(R.drawable.mycustom_toast_border_error)
+            INFO -> customToastroot.setBackgroundResource(R.drawable.mycustom_toast_border_info)
+            else -> { // Note the block
+                customToastroot.setBackgroundResource(R.drawable.mycustom_toast_border)
+            }
+        }
 
         customtoast.view = customToastroot
         customtoast.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM, 0, 0)
